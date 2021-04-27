e53c4e731571b134055648300da88b704957c846
Shadow7X <gabriel.alex251@gmail.com>
2021-04-28 00:15:15 +0100

    implemented search2 e inferencia por reflexao

--- PAProject/src/main/kotlin/Bibloteca.kt
+++ PAProject/src/main/kotlin/Bibloteca.kt
@@ -1,4 +1,12 @@
 import javax.swing.text.html.parser.Entity
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.full.valueParameters
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class TestCase
 
 abstract class JsonEntity(){
 
@@ -78,18 +86,24 @@ interface Visitor {
     fun visit(JN: JsonNumber) { }
     fun visit(JS: JsonString) { }
 }
-
-fun serializeAux(obj:JsonEntity):String{
+@TestCase
+fun serialize(obj:JsonEntity):String{
+    val result =serializeAux(obj,1)
+    return result
+}
+fun serializeAux(obj:JsonEntity,n:Int):String{
     var result=""
 
     when(obj){
-        is JsonObj -> {result+="{ ";var i=0; for(o in obj.children){if(i>0)result+=",";result+=" "+obj.childrenNames[i]+" :"+ serializeAux(o); i+=1};result+=" }"}
-        is JsonArray -> {result+="[";var i=0; for(o in obj.value){if(i>0)result+=",";result+=serializeAux(o);i+=1};result+="]"}
+        is JsonObj -> {result+="{ \n";var i=0; for(o in obj.children){if(i>0)result+=",\n";var j=0;while(j<n){result+="    ";j+=1};result+=" "+obj.childrenNames[i]+" :"+ serializeAux(o,n+3); i+=1};result+=" \n";var j=0;while(j<n-1){result+="    ";j+=1};result+="}"}
+        is JsonArray -> {result+="[ \n";var i=0; for(o in obj.value){if(i>0)result+=",\n";var j=0;while(j<n){result+="    ";j+=1};result+=serializeAux(o,n+1);i+=1};result+=" \n";var j=0;while(j<n-1){result+="    ";j+=1};result+="]"};
         is JsonNumber -> result+=" " + obj.value+" "
         is JsonString -> result+='"'+ obj.value+'"'
     }
     return result
+
 }
+@TestCase
 fun Search(c:JsonEntity):MutableList<String>{
     val getAllStrings = object : Visitor {
         val list: MutableList<String> = mutableListOf()
@@ -100,4 +114,88 @@ fun Search(c:JsonEntity):MutableList<String>{
     c.accept(getAllStrings)
     return getAllStrings.list
 
+}
+
+@TestCase //funcao que retorna todas as rodas com 0.5 de raio
+fun Search2(c:JsonEntity):MutableList<String>{
+    val result = object : Visitor {
+        val list: MutableList<String> = mutableListOf()
+        override fun endVisit(l: JsonObj) {
+            if(l.childrenNames.contains("raio")){
+                val n=l.childrenNames.indexOf("raio")
+                if(l.children.get(n).value==0.5)
+                    list.add(serialize(l))
+            }
+
+        }
+    }
+    c.accept(result)
+    return result.list
+}
+@TestCase
+fun inferênciaPorReflexão():JsonObj{
+    val clazzObj: KClass<JsonObj> = JsonObj::class
+    val clazzArray:KClass<JsonArray> = JsonArray::class
+    val clazzNumber: KClass<JsonNumber> = JsonNumber::class
+    val clazzString: KClass<JsonString> = JsonString::class
+
+    val Carro = clazzObj.primaryConstructor!!.call("Carro")
+
+    val marca = clazzString.primaryConstructor!!.call("BMW")
+
+    val setPriority: KFunction<*>? = clazzObj.declaredMemberFunctions.find { it.name.contains("setPriority") }
+
+    setPriority?.call(Carro,"marca",marca)
+
+    val Roda1 = clazzObj.primaryConstructor!!.call("Roda1")
+    val Roda2 = clazzObj.primaryConstructor!!.call("Roda2")
+    val Roda3 = clazzObj.primaryConstructor!!.call("Roda3")
+    val Roda4 = clazzObj.primaryConstructor!!.call("Roda4")
+
+    val Rodas = clazzArray.primaryConstructor!!.call()
+
+    setPriority?.call(Carro,"Rodas",Rodas)
+
+    val add: KFunction<*>? = clazzArray.declaredMemberFunctions.find{it.name.contains("add")}
+
+    add?.call(Rodas,Roda1)
+    add?.call(Rodas,Roda2)
+    add?.call(Rodas,Roda3)
+    add?.call(Rodas,Roda4)
+
+    val marcaR1 = clazzString.primaryConstructor!!.call("BMW")
+    val marcaR2 = clazzString.primaryConstructor!!.call("Ford")
+    val marcaR3 = clazzString.primaryConstructor!!.call("Nissan")
+    val marcaR4 = clazzString.primaryConstructor!!.call("Toyota")
+
+    setPriority?.call(Roda1,"marca",marcaR1)
+    setPriority?.call(Roda2,"marca",marcaR2)
+    setPriority?.call(Roda3,"marca",marcaR3)
+    setPriority?.call(Roda4,"marca",marcaR4)
+
+    val raioR1 = clazzNumber.primaryConstructor!!.call(0.5)
+    val larguraR1 = clazzNumber.primaryConstructor!!.call(0.2)
+
+    val raioR2 = clazzNumber.primaryConstructor!!.call(0.4)
+    val larguraR2 = clazzNumber.primaryConstructor!!.call(0.3)
+
+    val raioR3 = clazzNumber.primaryConstructor!!.call(0.4)
+    val larguraR3 = clazzNumber.primaryConstructor!!.call(0.2)
+
+    val raioR4 = clazzNumber.primaryConstructor!!.call(0.5)
+    val larguraR4 = clazzNumber.primaryConstructor!!.call(0.3)
+
+    setPriority?.call(Roda1,"raio",raioR1)
+    setPriority?.call(Roda1,"largura",larguraR1)
+
+    setPriority?.call(Roda2,"raio",raioR2)
+    setPriority?.call(Roda2,"largura",larguraR2)
+
+    setPriority?.call(Roda3,"raio",raioR3)
+    setPriority?.call(Roda3,"largura",larguraR3)
+
+    setPriority?.call(Roda4,"raio",raioR4)
+    setPriority?.call(Roda4,"largura",larguraR4)
+
+    return Carro
 }
--- PAProject/src/main/kotlin/PAProject.kt
+++ PAProject/src/main/kotlin/PAProject.kt
@@ -1,62 +1,73 @@
 
 
+ fun start():JsonObj{
+     val Carro:JsonObj = JsonObj("Carro")
+
+     val marca:JsonString = JsonString("BMW")
+
+     Carro.setPriority("marca",marca)
+
+     val Roda1:JsonObj = JsonObj("Roda1")
+     val Roda2:JsonObj = JsonObj("Roda2")
+     val Roda3:JsonObj = JsonObj("Roda3")
+     val Roda4:JsonObj = JsonObj("Roda4")
+
+     val Rodas:JsonArray = JsonArray()
+
+     Carro.setPriority("Rodas",Rodas)
+
+     Rodas.add(Roda1)
+     Rodas.add(Roda2)
+     Rodas.add(Roda3)
+     Rodas.add(Roda4)
+
+     val marcaR1:JsonString = JsonString("BMW")
+     val marcaR2:JsonString = JsonString("Ford")
+     val marcaR3:JsonString = JsonString("Nissan")
+     val marcaR4:JsonString = JsonString("Toyota")
+
+     Roda1.setPriority("marca",marcaR1)
+     Roda2.setPriority("marca",marcaR2)
+     Roda3.setPriority("marca",marcaR3)
+     Roda4.setPriority("marca",marcaR4)
+
+     val raioR1:JsonNumber = JsonNumber(0.5)
+     val larguraR1:JsonNumber = JsonNumber(0.2)
+
+     val raioR2:JsonNumber = JsonNumber(0.4)
+     val larguraR2:JsonNumber = JsonNumber(0.3)
+
+     val raioR3:JsonNumber = JsonNumber(0.4)
+     val larguraR3:JsonNumber = JsonNumber(0.2)
+
+     val raioR4:JsonNumber = JsonNumber(0.5)
+     val larguraR4:JsonNumber = JsonNumber(0.3)
+
+     Roda1.setPriority("raio",raioR1)
+     Roda1.setPriority("largura",larguraR1)
+
+     Roda2.setPriority("raio",raioR2)
+     Roda2.setPriority("largura",larguraR2)
+
+     Roda3.setPriority("raio",raioR3)
+     Roda3.setPriority("largura",larguraR3)
+
+     Roda4.setPriority("raio",raioR4)
+     Roda4.setPriority("largura",larguraR4)
+
+     return Carro
+ }
 fun main(){
-    val Carro:JsonObj = JsonObj("")
 
-    val marca:JsonString = JsonString("BMW")
+    val Carro:JsonObj=start()
+    val Carro2:JsonObj=inferênciaPorReflexão()
 
-    Carro.setPriority("marca",marca)
-
-    val Roda1:JsonObj = JsonObj("Roda1")
-    val Roda2:JsonObj = JsonObj("Roda2")
-    val Roda3:JsonObj = JsonObj("Roda3")
-    val Roda4:JsonObj = JsonObj("Roda4")
-
-    val Rodas:JsonArray = JsonArray()
-
-    Carro.setPriority("Rodas",Rodas)
-
-    Rodas.add(Roda1)
-    Rodas.add(Roda2)
-    Rodas.add(Roda3)
-    Rodas.add(Roda4)
-
-    val marcaR1:JsonString = JsonString("BMW")
-    val marcaR2:JsonString = JsonString("Ford")
-    val marcaR3:JsonString = JsonString("Nissan")
-    val marcaR4:JsonString = JsonString("Toyota")
-
-    Roda1.setPriority("marca",marcaR1)
-    Roda2.setPriority("marca",marcaR2)
-    Roda3.setPriority("marca",marcaR3)
-    Roda4.setPriority("marca",marcaR4)
-
-    val raioR1:JsonNumber = JsonNumber(0.5)
-    val larguraR1:JsonNumber = JsonNumber(0.2)
-
-    val raioR2:JsonNumber = JsonNumber(0.5)
-    val larguraR2:JsonNumber = JsonNumber(0.2)
-
-    val raioR3:JsonNumber = JsonNumber(0.5)
-    val larguraR3:JsonNumber = JsonNumber(0.2)
-
-    val raioR4:JsonNumber = JsonNumber(0.5)
-    val larguraR4:JsonNumber = JsonNumber(0.2)
-
-    Roda1.setPriority("raio",raioR1)
-    Roda1.setPriority("largura",larguraR1)
-
-    Roda2.setPriority("raio",raioR2)
-    Roda2.setPriority("largura",larguraR2)
-
-    Roda3.setPriority("raio",raioR3)
-    Roda3.setPriority("largura",larguraR3)
-
-    Roda4.setPriority("raio",raioR4)
-    Roda4.setPriority("largura",larguraR4)
-
-    println(serializeAux(Carro))
-    println(Search(Carro))
+    println("serializeAux:")
+    println(serialize(Carro2))
+    println("search:")
+    println(Search(Carro2))
+    println("search2:")
+    println(Search2(Carro2))
 
     /*println(serialize2(object1))
     println(serialize2(object2))
